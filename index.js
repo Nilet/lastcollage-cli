a0096e505150b5e40499b1eae19ddd8b7bb5f27a
Nilet <nilet@tutanota.com>
2022-12-09 19:29:48 -0300

    config is now json, supports array of users

--- index.js
+++ index.js
@@ -3,35 +3,43 @@ const path = require('path');
 const fetch = require('node-fetch');
 const { Headers } = fetch;
 
-const { savePath, params } = require('./config')
+const params = require("./config.json")
+
 
 let myHeaders = new Headers();
 myHeaders.append('Content-Type', 'application/json');
 
-let raw = JSON.stringify(params);
+const usernameArray = [...params.username]
+console.log(usernameArray)
 
-let requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
-
-fetch('https://lastcollage.io/api/collage', requestOptions)
-  .then(response => response.json())
-  .then(({ downloadPath }) => {
-    requestOptions = {
-      method: 'GET',
+for(let user of usernameArray){
+    let temp = params
+    temp.username = user
+    raw = JSON.stringify(temp)
+    console.log(raw)
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
       redirect: 'follow'
     };
+    const savePath= `${process.env.HOME}/collages/${user}.png`
+    fetch('https://lastcollage.io/api/collage', requestOptions)
+      .then(response => response.json())
+      .then(({ downloadPath }) => {
+        requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };
 
-    return fetch(`https://lastcollage.io/${downloadPath}`, requestOptions);
-  })
-  .then(response => new Promise((resolve, reject) => {
-    const fileStream = fs.createWriteStream(path.resolve(savePath));
-    response.body.pipe(fileStream)
-    response.body.on('error', reject);
-    response.body.on('finish', resolve);
-  }))
-  .then(console.log('success'))
-  .catch(error => console.log('error', error));
+        return fetch(`https://lastcollage.io/${downloadPath}`, requestOptions);
+      })
+      .then(response => new Promise((resolve, reject) => {
+        const fileStream = fs.createWriteStream(path.resolve(savePath));
+        response.body.pipe(fileStream)
+        response.body.on('error', reject);
+        response.body.on('finish', resolve);
+      }))
+      .then(console.log('success'))
+      .catch(error => console.log('error', error));
+}
