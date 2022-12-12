e46074516375ab85342fc72e8acb0f1c345e8237
Nilet <nilet@tutanota.com>
2022-12-11 23:34:41 -0300

    cleaned up some mess

--- index.js
+++ index.js
@@ -2,43 +2,46 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const { Headers } = fetch;
-const params = require("./config.json")
+const params = require('./config.json');
+const usernameArray = [...params.username];
 
-//Change this to change where the files are stored
-let savePath = `${process.env.HOME}/collages/`
+// Change this to change where the files are stored
+const savePath = `${process.env.HOME}/collages/`;
 
-let myHeaders = new Headers();
-myHeaders.append('Content-Type', 'application/json');
 
-const usernameArray = [...params.username]
+const createCollage = async (user) => {
+  let temp = params;
+  temp.username = user;
+  raw = JSON.stringify(temp);
+  let requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: raw,
+    redirect: 'follow',
+  };
 
-for(let user of usernameArray){
-    let temp = params
-    temp.username = user
-    raw = JSON.stringify(temp)
-    let requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
+  const tempPath = `${savePath}${user}.png`;
+  try {
+    const response = await fetch('https://lastcollage.io/api/collage', requestOptions);
+    const { downloadPath } = await response.json();
+    requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
     };
-    const tempPath= savePath.concat(user, ".png");
-    fetch('https://lastcollage.io/api/collage', requestOptions)
-      .then(response => response.json())
-      .then(({ downloadPath }) => {
-        requestOptions = {
-          method: 'GET',
-          redirect: 'follow'
-        };
 
-        return fetch(`https://lastcollage.io/${downloadPath}`, requestOptions);
-      })
-      .then(response => new Promise((resolve, reject) => {
-        const fileStream = fs.createWriteStream(path.resolve(tempPath));
-        response.body.pipe(fileStream)
-        response.body.on('error', reject);
-        response.body.on('finish', resolve);
-      }))
-      .then(console.log('success'))
-      .catch(error => console.log('error', error));
+    const fileResponse = await fetch(`https://lastcollage.io/${downloadPath}`, requestOptions);
+    const fileStream = fs.createWriteStream(path.resolve(tempPath));
+    fileResponse.body.pipe(fileStream);
+    fileResponse.body.on('error', (error) => {
+      console.log('Error:', error);
+      reject(error);
+    });
+    fileResponse.body.on('finish', () => console.log("Collage created"));
+  } catch (error) {
+    console.log('Error:', error);
+  }
+};
+
+for (const user of usernameArray) {
+  createCollage(user);
 }
